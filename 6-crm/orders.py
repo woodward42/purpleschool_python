f31d22f340bb0b25cfb2f444b43975a3b3f32998
woodward42 <woodward42@users.noreply.github.com>
2026-03-02 15:12:44 +0700

    [dz done] chapter13

--- 6-crm/orders.py
+++ 6-crm/orders.py
@@ -1,4 +1,5 @@
-from typing import TypedDict
+from typing import TypedDict, Optional
+from datetime import datetime
 
 # возможные статусы заказа
 ORDER_STATES = {"new", "in_progress", "done", "cancelled"}
@@ -10,22 +11,61 @@ class Order(TypedDict):
     amount: float
     email: str
     status: str
-    tags: set[str]
+    tags: Optional[set[str]]
     created_at: str
-    due: str | None
-    closed_at: str
+    due: Optional[str]
+    closed_at: Optional[str]
 
-def create_order():
-    pass
+
+# Реализовать все методы:
+
+# create_order
+# list_orders
+# edit_order
+# remove_order
+# Хранить список заказов в переменной
+
+# переменная для хранения заказов
+orders: list[Order] = []
+
+
+def create_order(
+    title: str,
+    amount: float,
+    email: str,
+    status: str = "new",
+    tags: Optional[set[str]] = None,
+    due: Optional[str] = None,
+):
+    new_order: Order = {
+        "id": len(orders) + 1,
+        "title": title,
+        "amount": amount,
+        "email": email,
+        "status": status,
+        "tags": tags,
+        "created_at": str(datetime.now()),
+        "due": due,
+        "closed_at": None,
+    }
+    orders.append(new_order)
+    return new_order
 
 
 def list_orders():
-    pass
+    print(f"Список заказов:\n {orders}")
 
 
-def edit_order():
-    pass
+def edit_order(order_id: int):
+    for order in orders:
+        if order["id"] == order_id:
+            # тут логика изменения заказа, не описана в дз
+            pass
 
 
-def remove_order():
-    pass
+def remove_order(order_id: int):
+    for i, order in enumerate(orders):
+        if order["id"] == order_id:
+            orders.pop(i)
+            return True
+    return False
